941f94d1c787b825aa0a445b24347032c37094a6
inkognitorecords <info@inkognitorecords.com>
2026-04-23 03:29:35 +0400

    Update Puppeteer cache directory path

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,6 +1,3 @@
-const { join } = require('path');
-
-/** @type {import('puppeteer').Configuration} */
 module.exports = {
-  cacheDirectory: join(__dirname, 'node_modules', '.puppeteer_cache'),
+  cacheDirectory: '/opt/render/project/src/node_modules/.puppeteer_cache',
 };
